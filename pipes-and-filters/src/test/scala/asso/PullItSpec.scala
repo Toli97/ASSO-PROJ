10c5844e11ec7dadabd741c097b452b29dca8712
ros <rk65536@protonmail.com>
2019-05-09 19:51:19 +0100

    add error checking tests

--- pipes-and-filters/src/test/scala/asso/PullItSpec.scala
+++ pipes-and-filters/src/test/scala/asso/PullItSpec.scala
@@ -10,11 +10,10 @@ object RandomName {
   private var curr = 0
 
   def nextName: String = {
-    curr += 1;
+    curr += 1
     s"ASSO-Proj-test-$curr"
   }
 }
-
 
 class ItSpec extends FlatSpec with Matchers {
   def createFile(body: String = ""): String = {
@@ -25,9 +24,16 @@ class ItSpec extends FlatSpec with Matchers {
     file.toString
   }
 
-  def readFileAsNums(filepath: String): Seq[Long] = Source.fromFile(filepath).mkString
-    .split("\\s+")
-    .map(word => word.toLong)
+  def readFileAsNums(filepath: String): Seq[Long] = {
+    val str = Source.fromFile(filepath).mkString
+
+    if (str == "") {
+      return Seq()
+    }
+
+    str.split("\\s+")
+      .map(word => word.toLong)
+  }
 
 
   "The algorithm" should "should work" in {
@@ -36,7 +42,7 @@ class ItSpec extends FlatSpec with Matchers {
     val in3 = createFile("2 4 9 12 11 15 1")
     val in4 = createFile("3 2 3 10 2 5")
     val out = createFile()
-    val expectedNums = Seq(8, -1, -3)
+    val expectedNums = Seq(8, -1, -3) // manually calculated to be correct
 
     Main.main(Array("pull", out, in1, in2, in3, in4))
 
@@ -44,6 +50,43 @@ class ItSpec extends FlatSpec with Matchers {
     expectedNums shouldEqual actualNums
   }
 
+  "The algorithm with empty inputs" should "should produce empty output" in {
+    val expectedNums = Seq()
 
+    val out = createFile()
+    Main.main(Array("pull", out, createFile(), createFile(), createFile(), createFile()))
+
+    val actualNums = readFileAsNums(out)
+    expectedNums shouldEqual actualNums
+  }
+
+  "An invalid command" should "fail" in {
+    val invalidCommand = "ASDFGQWE"
+    try {
+      Main.main(Array(invalidCommand))
+      fail()
+    } catch {
+      case e: IllegalArgumentException => e.getMessage should include(invalidCommand)
+    }
+  }
+
+  "An invalid number of commands" should "fail" in {
+    try {
+      Main.main(Array("pull", createFile(), createFile()))
+      fail()
+    } catch {
+      case e: IllegalArgumentException => e.getMessage should include("must specify one output")
+    }
+  }
+
+  "Invalid text in a source file" should "fail" in {
+    val invalidText = "awwe"
+    try {
+      Main.main(Array("pull", createFile(), createFile("2"), createFile("2"), createFile("2"), createFile(invalidText)))
+      fail()
+    } catch {
+      case e: NumberFormatException => e.getMessage should include(invalidText)
+    }
+  }
 
 }
